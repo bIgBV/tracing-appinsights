5487fae8bf6264321406172d193f92bb2f990dd3
bIgBV <bhargav.voleti93@gmail.com>
2021-08-25 16:54:41 -0700

    Start tracking traces

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,56 @@
-use appinsights::TelemetryClient;
-use tracing::Subscriber;
+use appinsights::{
+    telemetry::{SeverityLevel, Telemetry, TraceTelemetry},
+    InMemoryChannel, TelemetryClient,
+};
+use tracing::{field::Visit, Level, Subscriber};
 use tracing_subscriber::Layer;
 
-pub struct AppInsightsLayer<C> {
-    client: TelemetryClient<C>,
+fn into_severity_level(level: &tracing::Level) -> SeverityLevel {
+    match *level {
+        Level::TRACE => SeverityLevel::Verbose,
+        Level::DEBUG => SeverityLevel::Information,
+        Level::INFO => SeverityLevel::Warning,
+        Level::WARN => SeverityLevel::Error,
+        Level::ERROR => SeverityLevel::Critical,
+    }
 }
 
-impl<C> AppInsightsLayer<C> {
-    pub fn new(client: TelemetryClient<C>) -> Self {
+pub struct AppInsightsLayer {
+    client: TelemetryClient<InMemoryChannel>,
+}
+
+impl AppInsightsLayer {
+    pub fn new(client: TelemetryClient<InMemoryChannel>) -> Self {
         AppInsightsLayer { client }
     }
 }
 
-impl<S, C> Layer<S> for AppInsightsLayer<C>
+impl<S> Layer<S> for AppInsightsLayer
 where
     S: Subscriber,
-    C: 'static,
 {
     fn on_event(
         &self,
         event: &tracing::Event<'_>,
         _ctx: tracing_subscriber::layer::Context<'_, S>,
     ) {
-        println!("Got an event: {:?}", event);
+        let mut telemetry = TraceTelemetry::new(
+            "figuring out how to get the message",
+            into_severity_level(event.metadata().level()),
+        );
+
+        event.record(&mut SpanEventVisitor(&mut telemetry));
+
+        self.client.track(telemetry);
+    }
+}
+
+struct SpanEventVisitor<'a>(&'a mut TraceTelemetry);
+
+impl<'a> Visit for SpanEventVisitor<'a> {
+    fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
+        self.0
+            .properties_mut()
+            .insert(field.to_string(), format!("{:?}", value));
     }
 }
